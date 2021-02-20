7883469cf6ad3fa19d42242ea93a892263a44ec5
AHMED RABIE <ahmed.rabie.mahmoud.96@gmail.com>
2021-02-20 16:47:52 +0200

    Shopping Cart Boilerplate

---
+++ routes/carts.js
@@ -0,0 +1,11 @@
+const express = require('express');
+
+const router = express.Router();
+
+// Recive a post request to ad an item to a cart
+
+// Recive a GET request to show all items in a cart
+
+// Receive a post request to delete on an item from a cart
+
+module.exports = router;
