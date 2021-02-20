444b28ce8c5bad83fa6be69ce3137c95fd0448b8
AHMED RABIE <ahmed.rabie.mahmoud.96@gmail.com>
2021-02-20 17:06:56 +0200

    Submission Options

--- routes/carts.js
+++ routes/carts.js
@@ -4,6 +4,11 @@ const router = express.Router();
 
 // Recive a post request to ad an item to a cart
 
+router.post('/cart/products', (req, res) => {
+	console.log(req.body.productId);
+
+	res.send('Product added to cart');
+});
 // Recive a GET request to show all items in a cart
 
 // Receive a post request to delete on an item from a cart
